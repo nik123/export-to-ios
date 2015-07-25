c08c58e38b8e7461967c3341a117ed356ccb97ae
Nikita Kodenko <kodenkonikita@gmail.com>
2015-07-25 18:25:16 +0600

    Initial commit

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 A Photoshop script for exporting assets for iOS projects at @1x, @2x and @3x.
 
-The script works by duplicating the selected layer (or layergroup) to a new document, then scaling it to each of the 3 common iOS sizes (@1x, @2x, and @3x) and then placing the files inside a folder next to the PSD.
+The script works by duplicating current document, then scaling it to each of the 3 common iOS sizes (@1x, @2x, and @3x) and then placing the files inside a folder next to the PSD.
+
+The script works almost the same with original one (https://github.com/UncorkedStudios/export-to-ios) but exports not just one layer but whole document.
 
 ## Installation
 1. Download the script here
--- us-export-to-ios.jsx
+++ us-export-to-ios.jsx
@@ -1,150 +1,118 @@
-/*!
- * iOS Assets for Photoshop
- * =============================
- *
- * Version: 1.0.0
- * Author: Gaston Figueroa (Uncorked Studios)
- * Site: uncorkedstudios.com
- * Licensed under the MIT license
- */
-
-
 // Photoshop variables
 var docRef = app.activeDocument,
-	activeLayer = docRef.activeLayer,
-	activeLayer2,
-	newWidth, 
-	newHeight,
-	docName = docRef.name;
+    activeLayer = docRef.activeLayer,
+    activeLayer2,
+    newWidth, 
+    newHeight,
+    docNameNoExt = filenameNoExt(docRef.name);
 
 var scaleFactors = {
-	'@3x': 1,
-	'@2x': 1.5,
-	'@1x': 3,
+    '@3x': 1,
+    '@2x': 1.5,
+    '@1x': 3,
 };
 
 // Run main function
 init();
 
-// The other functions
 function init() {
-	if(!isDocumentNew()) {
-		for(var dpi in scaleFactors) {
-			saveFunc(dpi);
-		}
-	} else {
-		alert("Please save your document before running this script.");
-	}
+    if(!isDocumentNew()) {
+        for(var dpi in scaleFactors) {
+           saveFunc(dpi);
+        }
+    } else {
+        alert("Please save your document before running this script.");
+    }
+}
+
+function filenameNoExt(filename) {
+    //Locate the final position of the final . before the extension.
+    var dotPos = filename.lastIndexOf( "." ) ;
+
+    if ( dotPos > -1 ) {
+        
+        return filename.substr( 0 , dotPos );
+    }
+    
+    //if dotPos is more than -1 then filename does not contain extension
+    return filename;
 }
 
 // Test if the document is new (unsaved)
 // http://2.adobe-photoshop-scripting.overzone.net/determine-if-file-has-never-been-saved-in-javascript-t264.html
-
 function isDocumentNew(doc){
-	// assumes doc is the activeDocument
-	cTID = function(s) { return app.charIDToTypeID(s); }
-	var ref = new ActionReference();
-	ref.putEnumerated( cTID("Dcmn"),
-	cTID("Ordn"),
-	cTID("Trgt") ); //activeDoc
-	var desc = executeActionGet(ref);
-	var rc = true;
-		if (desc.hasKey(cTID("FilR"))) { //FileReference
-		var path = desc.getPath(cTID("FilR"));
-		
-		if (path) {
-			rc = (path.absoluteURI.length == 0);
-		}
-	}
-	return rc;
-};
-
-function resizeDoc(document, scale) {
-	var calcWidth  = activeLayer.bounds[2] - activeLayer.bounds[0], // Get layer's width
-		calcHeight = activeLayer.bounds[3] - activeLayer.bounds[1]; // Get layer's height	
-
-	// Resize assets
-	newHeight = Math.floor(calcHeight / scaleFactors[scale]);
-	newWidth = Math.floor(calcWidth / scaleFactors[scale]);
-
-	// Resize temp document using Bicubic interpolation
-	resizeLayer(newWidth);
-
-	// Merge all layers inside the temp document
-	activeLayer2.merge();
-}
-
-
-// document.resizeImage doesn't seem to support scalestyles so we're using this workaround from http://ps-scripts.com/bb/viewtopic.php?p=14359
-function resizeLayer(newWidth) {
-	var idImgS = charIDToTypeID( "ImgS" );
-	var desc2 = new ActionDescriptor();
-	var idWdth = charIDToTypeID( "Wdth" );
-	var idPxl = charIDToTypeID( "#Pxl" );
-	desc2.putUnitDouble( idWdth, idPxl, newWidth);
-	var idscaleStyles = stringIDToTypeID( "scaleStyles" );
-	desc2.putBoolean( idscaleStyles, true );
-	var idCnsP = charIDToTypeID( "CnsP" );
-	desc2.putBoolean( idCnsP, true );
-	var idIntr = charIDToTypeID( "Intr" );
-	var idIntp = charIDToTypeID( "Intp" );
-	var idBcbc = charIDToTypeID( "Bcbc" );
-	desc2.putEnumerated( idIntr, idIntp, idBcbc );
-	executeAction( idImgS, desc2, DialogModes.NO );
-}
-
-function dupToNewFile() {
-	// TO DO: Target all files ending with '#'
-	var fileName = activeLayer.name.replace(/\.[^\.]+$/, ''), 
-		calcWidth  = Math.ceil(activeLayer.bounds[2] - activeLayer.bounds[0]),
-		calcHeight = Math.ceil(activeLayer.bounds[3] - activeLayer.bounds[1]),
-		docResolution = docRef.resolution,
-		document = app.documents.add(calcWidth, calcHeight, docResolution, fileName, NewDocumentMode.RGB,
-		DocumentFill.TRANSPARENT);
-
-	app.activeDocument = docRef;
-
-	// Duplicated selection to a temp document
-	activeLayer.duplicate(document, ElementPlacement.INSIDE);
-
-	// Set focus on temp document
-	app.activeDocument = document;
-
-	// Assign a variable to the layer we pasted inside the temp document
-	activeLayer2 = document.activeLayer;
-
-	// Center the layer
-	activeLayer2.translate(-activeLayer2.bounds[0],-activeLayer2.bounds[1]);
+    // assumes doc is the activeDocument
+    cTID = function(s) { return app.charIDToTypeID(s); }
+    var ref = new ActionReference();
+    ref.putEnumerated( cTID("Dcmn"),
+    cTID("Ordn"),
+    cTID("Trgt") ); //activeDoc
+    var desc = executeActionGet(ref);
+    var rc = true;
+        if (desc.hasKey(cTID("FilR"))) { //FileReference
+        var path = desc.getPath(cTID("FilR"));
+        
+        if (path) {
+            rc = (path.absoluteURI.length == 0);
+        }
+    }
+    return rc;
 }
 
 function saveFunc(dpi) {
-	dupToNewFile();
-	var docRef2 = app.activeDocument;
-	resizeDoc(docRef2, dpi);
+    app.activeDocument = docRef;
+    duplicateImage(false);
+    resizeActiveDoc(dpi);
 
-	var Name = docRef2.name.replace(/\.[^\.]+$/, ''), 
-		Ext = decodeURI(docRef2.name).replace(/^.*\./,''), 
-		Path = docRef.path,
-		folder = Folder(Path + '/' + docName + '-assets/');
-		
-	if(!folder.exists) {
-		folder.create();
-	}
+    var path = docRef.path; 
+    var folder = Folder(path + '/' + docNameNoExt + '-assets/');
+    if(!folder.exists) {
+        folder.create();
+    }
+    // Name the new asset
+    var saveFile = File(folder + "/" + docNameNoExt + (dpi === '@1x' ? '' : dpi) + ".png");
 
-	// Name the new asset
-	var saveFile = File(folder + "/" + Name + (dpi === '@1x' ? '' : dpi) + ".png");
+    var sfwOptions = new ExportOptionsSaveForWeb(); 
+        sfwOptions.format = SaveDocumentType.PNG; 
+        sfwOptions.includeProfile = false; 
+        sfwOptions.interlaced = 0; 
+        sfwOptions.optimized = true; 
+        sfwOptions.quality = 100;
+        sfwOptions.PNG8 = false;
 
-	var sfwOptions = new ExportOptionsSaveForWeb(); 
-		sfwOptions.format = SaveDocumentType.PNG; 
-		sfwOptions.includeProfile = false; 
-		sfwOptions.interlaced = 0; 
-		sfwOptions.optimized = true; 
-		sfwOptions.quality = 100;
-		sfwOptions.PNG8 = false;
+    // Export the layer as a PNG
+    activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, sfwOptions);
 
-	// Export the layer as a PNG
-	activeDocument.exportDocument(saveFile, ExportType.SAVEFORWEB, sfwOptions);
+    // Close the document without saving
+    activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+}
 
-	// Close the document without saving
-	activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+// Duplicate image. Merged shows if layers groups should be merged
+function duplicateImage(merged) {
+    var desc1 = new ActionDescriptor();
+    var ref1 = new ActionReference();
+    ref1.putEnumerated(cTID('Dcmn'), cTID('Ordn'), cTID('Frst'));
+    desc1.putReference(cTID('null'), ref1);
+    if (merged) {desc1.putBoolean(cTID('Mrgd'), true);}
+    executeAction(cTID('Dplc'), desc1, DialogModes.NO);
+}
+
+function resizeActiveDoc(scale) {
+    // get a reference to the current (active) document and store it in a variable named "doc"
+    doc = app.activeDocument;
+
+    // change the color mode to RGB.  Important for resizing GIFs with indexed colors, to get better results
+    doc.changeMode(ChangeMode.RGB);  
+
+    // these are our values for the end result width and height (in pixels) of our image
+    var newHeight = Math.floor(app.activeDocument.height / scaleFactors[scale]);
+    var newWidth = Math.floor(app.activeDocument.width / scaleFactors[scale]);
+
+    // do the resizing.  if height > width (portrait-mode) resize based on height.  otherwise, resize based on width
+    if (doc.height > doc.width) {
+        doc.resizeImage(null,UnitValue(newHeight,"px"),null,ResampleMethod.BICUBIC);
+    }
+    else {
+        doc.resizeImage(UnitValue(newWidth,"px"),null,null,ResampleMethod.BICUBIC);
+    }
 }
